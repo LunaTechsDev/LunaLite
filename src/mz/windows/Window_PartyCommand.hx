c13afa5af94a99a5dece5efda3fea024a5cd1d60
Kino  Rose <kinoarose@gmail.com>
2020-07-11 20:12:45 -0400

    Add Window_party and WIndow_actor command

---
+++ src/mz/windows/Window_PartyCommand.hx
@@ -0,0 +1,14 @@
+package mz.windows;
+
+/**
+ * -----------------------------------------------------------------------------
+ * Window_PartyCommand
+ *
+ * The window for selecting whether to fight or escape on the battle screen.
+ * @class Window_PartyCommand
+ */
+ extern class Window_PartyCommand extends Window_Command {
+  public function new();
+
+  public function setup(): Void;
+}
