ef2db81d1f0e6004490a851ba8182a64d4ab8b6a
Kino  Rose <kinoarose@gmail.com>
2020-07-12 11:33:18 -0400

    Fill out Scene_Menu, Scene_MenuBase

--- src/mz/scenes/Scene_MenuBase.hx
+++ src/mz/scenes/Scene_MenuBase.hx
@@ -1,73 +1,82 @@
 package mz.scenes;
 
-import mz.windows.Window_EquipStatus;
-import mz.windows.Window_EquipItem;
-import mz.windows.Window_EquipSlot;
-import mz.windows.Window_EquipCommand;
+import mz.core.Sprite;
+import mz.windows.Window_Help;
+import mz.objects.Game_Actor;
 
 /**
  * The super class of all menu-type scenes
  */
 @:native("Scene_MenuBase")
 extern class Scene_MenuBase extends Scene_Base {
-	private var _statusWindow:Window_EquipStatus;
-	private var _commandWindow:Window_EquipCommand;
-	private var _slotWindow:Window_EquipSlot;
-	private var _itemWindow:Window_EquipItem;
-
-	/**
-	 * Creates the status window within the equip scene.
-	 *
-	 * @memberof Scene_Equip
-	 */
-	public function createStatusWindow():Void;
-
-	/**
-	 * Creates the status window within the equip scene.
-	 *
-	 * @memberof Scene_Equip
-	 */
-	public function createCommandWindow():Void;
-
-	/**
-	 * Creates the slot window within the  equip scene.
-	 *
-	 * @memberof Scene_Equip
-	 */
-	public function createSlotWindow():Void;
-
-	/**
-	 * Creates the item window within the equip scene.
-	 *
-	 * @memberof Scene_Equip
-	 */
-	public function createItemWindow():Void;
-
-	/**
-	 * Refreshes the actor within the equip scene.
-	 *
-	 * @memberof Scene_Equip
-	 */
-	public function refreshActor():Void;
-
-	/**
-	 * Handler for the equip command.
-	 *
-	 * @memberof Scene_Equip
-	 */
-	public function commandEquip():Void;
-
-	/**
-	 * Handler for the optimize command.
-	 *
-	 * @memberof Scene_Equip
-	 */
-	public function commandOptimize():Void;
-
-	public function commandClear():Void;
-	public function onSlotOk():Void;
-	public function onSlotCancel():Void;
-	public function onItemOk():Void;
-	public function onItemCancel():Void;
-	public function onActorChange():Void;
+	    /**
+     * The current actor selected in the base menu scene.
+     * 
+     * @protected
+     * @type {Game_Actor}
+     * @memberof Scene_MenuBase
+     */
+		 private var _actor: Game_Actor;
+		 /**
+			* The background sprite used to display the scene background.
+			* 
+			* @protected
+			* @type {Sprite}
+			* @memberof Scene_MenuBase
+			*/
+		 private var  _backgroundSprite: Sprite;
+		 /**
+			* The help window attached to the base menu scene.
+			* 
+			* @protected
+			* @type {Window_Help}
+			* @memberof Scene_MenuBase
+			*/
+		 private var _helpWindow: Window_Help;
+ 
+		 /**
+			* Returns the current game actor.
+			* 
+			* @returns {Game_Actor} 
+			* @memberof Scene_MenuBase
+			*/
+		 public function actor(): Game_Actor;
+		 /**
+			* Updates the current actor.
+			* 
+			* @memberof Scene_MenuBase
+			*/
+		 public function updateActor(): Void;
+		 /**
+			* Creates the background of the base menu scene.
+			* 
+			* @memberof Scene_MenuBase
+			*/
+		 public function createBackground(): Void;
+		 /**
+			* Sets the background opacity of the base menu scene background.
+			* 
+			* @param {number} opacity 
+			* @memberof Scene_MenuBase
+			*/
+		 public function setBackgroundOpacity(opacity: Int): Void;
+		 /**
+			* Creates the help window.
+			* 
+			* @memberof Scene_MenuBase
+			*/
+		 public function createHelpWindow(): Void;
+		 /**
+			* Moves to the next actor in the party.
+			* 
+			* @memberof Scene_MenuBase
+			*/
+		 public function nextActor(): Void;
+		 /**
+			* Moves to the previous actor in the party.
+			* 
+			* @memberof Scene_MenuBase
+			*/
+		 public function previousActor(): Void;
+		 public function onActorChange(): Void;	
 }
