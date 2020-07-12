382ba0d665f46985f8bebfb9476a5731139c1c30
Kino  Rose <kinoarose@gmail.com>
2020-07-11 20:14:54 -0400

    Add Window_BattleItem and Window_BattleSkill

---
+++ src/mz/windows/Window_BattleItem.hx
@@ -0,0 +1,16 @@
+package mz.windows;
+
+import mz.types.RPG.UsableItem;
+
+/**
+ * -----------------------------------------------------------------------------
+ * Window_BattleItem
+ *
+ * The window for selecting an item to use on the battle screen.
+ * @class Window_BattleItem
+ */
+ extern class Window_BattleItem extends Window_ItemList {
+  public function new(x: Int, y: Int, width: Int, height: Int);
+
+  public function includes(item: UsableItem): Bool;
+}
