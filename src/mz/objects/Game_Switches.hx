64484ab11403f1bcdfd6913bfc03de12b8611cc2
Kino  Rose <kinoarose@gmail.com>
2020-07-13 22:35:26 -0400

    Fill  out game_Switches, Game_SelfSwitches, Game_Item

--- src/mz/objects/Game_Switches.hx
+++ src/mz/objects/Game_Switches.hx
@@ -3,5 +3,10 @@ package mz.objects;
 
 @:native("Game_Switches")
 extern class Game_Switches {
+  private var _data: Array<Bool>;
 
+    public function clear(): Void;
+    public function value(switchId: Int): Bool;
+    public function setValue(switchId: Int, value: Bool): Void;
+    public function onChange(): Void;
 }
