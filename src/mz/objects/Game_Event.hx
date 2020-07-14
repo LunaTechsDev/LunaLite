813aacbbd68dcd586e632c0a0285cee10a3f51f9
Kino  Rose <kinoarose@gmail.com>
2020-07-13 22:50:04 -0400

    Fill out game _ event class

--- src/mz/objects/Game_Event.hx
+++ src/mz/objects/Game_Event.hx
@@ -1,5 +1,10 @@
 package mz.objects;
 
+import mz.types.RPG.MoveRoute;
+import mz.types.RPG.EventPage;
+import mz.types.RPG.Event;
+import mz.types.RPG.EventCommand;
+
 
 /**
  * THe game object class for an event. It contains functionality
@@ -22,19 +27,150 @@ extern class Game_Event extends Game_Character {
 
   public function new(mapId:Int, eventId:Int);
 
-  public function initMembers():Void;
-  /**
-   * Returns the event id of the game event
-   * @return Int
-   */
-  public function eventId():Int;
-  /**
-   * Locks the game event.
-   */
-  public function lock():Void;
-  /**
-   * Unlocks the game event.
-   */
-  public function unlock():Void;
-  public function updateStop():Void;
+    public function initMembers(): Void;
+    /**
+     * Returns the event id of the game event.
+     * 
+     * @returns {Int} 
+     * @memberof Game_Event
+     */
+    public function eventId(): Int;
+    /**
+     * Returns the event database information.
+     * 
+     * @returns {RPG.Event} 
+     * @memberof Game_Event
+     */
+    public function event(): Event;
+    /**
+     * Returns the event page created in the database.
+     * 
+     * @returns {RPG.EventPage} 
+     * @memberof Game_Event
+     */
+    public function page(): EventPage;
+    /**
+     * Returns the list of event commands on the current page of the game event.
+     * 
+     * @returns {Array<RPG.EventCommand>} 
+     * @memberof Game_Event
+     */
+    public function list(): Array<EventCommand>;
+    public function isCollidedWithCharacters(x: Int, y: Int): Bool;
+    public function isCollidedWithEvents(x: Int, y: Int): Bool;
+    /**
+     * Returns true if the event has collided with the player character
+     * at the specified x and y coordinates.
+     * @param {Int} x 
+     * @param {Int} y 
+     * @returns {Bool} 
+     * @memberof Game_Event
+     */
+    public function isCollidedWithPlayerCharacters(x: Int, y: Int): Bool;
+    /**
+     * Locks the game event.
+     * 
+     * @memberof Game_Event
+     */
+    public function lock(): Void;
+    /**
+     * Unlocks the game eveent.
+     * 
+     * @memberof Game_Event
+     */
+    public function unlock(): Void;
+    public function updateStop(): Void;
+    /**
+     * Updates the self movement of the game event.
+     * 
+     * @memberof Game_Event
+     */
+    public function updateSelfMovement(): Void;
+    public function stopCountThreshold(): Int;
+    public function moveTypeRandom(): Void;
+    public function moveTypeTowardPlayer(): Void;
+    /**
+     * Returns true if the game event is near the player.
+     * 
+     * @returns {Bool} 
+     * @memberof Game_Event
+     */
+    public function isNearThePlayer(): Bool;
+    public function moveTypeCustom(): Void;
+    /**
+     * Returns true if the event is staring.
+     * 
+     * @returns {Bool} 
+     * @memberof Game_Event
+     */
+    public function isStarting(): Bool;
+    /**
+     * Clears the starting flag.
+     * 
+     * @memberof Game_Event
+     */
+    public function clearStartingFlag(): Void;
+    public function isTriggerIn(triggers: Array<Int>): Bool;
+    /**
+     * Starts the game event.
+     * 
+     * @memberof Game_Event
+     */
+    public function start(): Void;
+    /**
+     * Erases the event.
+     * 
+     * @memberof Game_Event
+     */
+    public function erase(): Void;
+    /**
+     * Refreshes the game event.
+     * 
+     * @memberof Game_Event
+     */
+    public function refresh(): Void;
+    /**
+     * Finds the proper page index of the game event for
+     * event command processing.
+     * @returns {Int} 
+     * @memberof Game_Event
+     */
+    public function findProperPageIndex(): Int;
+    public function meetsConditions(page: EventPage): Bool;
+    /**
+     * Sets up the event page.
+     * 
+     * @memberof Game_Event
+     */
+    public function setupPage(): Void;
+    /**
+     * Clears the page settings of the game event.
+     * 
+     * @memberof Game_Event
+     */
+    public function clearPageSettings(): Void;
+    /**
+     * Sets up the event page settings.
+     * 
+     * @memberof Game_Event
+     */
+    public function setupPageSettings(): Void;
+    public function isOriginalPattern(): Bool;
+    /**
+     * Resets the pattern of the game event.
+     * 
+     * @memberof Game_Event
+     */
+    public function resetPattern(): Void;
+    public function checkEventTriggerAuto(): Void;
+    public function update(): Void;
+    public function updateParallel(): Void;
+    public function locate(x: Int, y: Int): Void;
+    /**
+     * Forces the game event to move along the specified route.
+     * 
+     * @param {RPG.MoveRoute} moveRoute 
+     * @memberof Game_Event
+     */
+    public function forceMoveRoute(moveRoute: MoveRoute): Void;
 }
