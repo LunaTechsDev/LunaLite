35c48dd631c454889019cc744780854f6ef6df0a
Kino  Rose <kinoarose@gmail.com>
2020-07-14 21:01:35 -0400

    Game_Actors and Game_Map fill out

--- src/mz/objects/Game_Actors.hx
+++ src/mz/objects/Game_Actors.hx
@@ -2,5 +2,14 @@ package mz.objects;
 
 @:native("Game_Actors")
 extern class Game_Actors {
-  private var _data:Array<Game_Actor>;
+ private var _data:Array<Game_Actor>;
+
+ /**
+  * Returns the actor with the specified id.
+  *
+  * @param {number} actorId
+  * @returns {Game_Actor}
+  * @memberof Game_Actors
+  */
+ public function actor(actorId:Int):Game_Actor;
 }
