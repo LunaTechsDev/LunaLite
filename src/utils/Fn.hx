bb3d68ca8b945ebe8fd046b4f04fce91f7923bb9
Kino  Rose <kinoarose@gmail.com>
2020-07-18 19:15:07 -0400

    Fix haxe formatting

--- src/utils/Fn.hx
+++ src/utils/Fn.hx
@@ -9,7 +9,7 @@ import Type;
 typedef Self = String;
 
 class Fn {
- public static var self(get, null):Any;
+ public static var self(get, null): Any;
 
  /**
   * Wrapper for JavaScript this
@@ -22,7 +22,7 @@ class Fn {
   * Runs a JavaScript evaluation expression.
   * @param evaluation
   */
- public static inline function eval(evaluation:String) {
+ public static inline function eval(evaluation: String) {
   return js.Lib.eval(evaluation);
  }
 
@@ -30,29 +30,29 @@ class Fn {
   * Embeds a JavaScript evaluation into the output program.
   * @param evaluation
   */
- public static inline function embedEval(evaluation:String) {
+ public static inline function embedEval(evaluation: String) {
   return Syntax.code("eval({0})", evaluation);
  }
 
  @:keep
- public static inline function proto<T>(obj:Class<T>):TBox<T> {
+ public static inline function proto<T>(obj: Class<T>): TBox<T> {
   return Syntax.field(obj, "prototype");
  }
 
- public static inline function setPrProp(obj:Any, fieldName:String, value:Any) {
+ public static inline function setPrProp(obj: Any, fieldName: String, value: Any) {
   Syntax.code("{0}.@{1}@ = {2}", proto(obj), (fieldName), value);
  }
 
- public static inline function setPrPropVoidFn(obj:Any, fieldName:String, value:(Any) -> Void) {
+ public static inline function setPrPropVoidFn(obj: Any, fieldName: String, value: (Any) -> Void) {
   Syntax.code("{0}.@{1}@ = {2}", proto(obj), (fieldName), value);
  }
 
- public static inline function getPrProp(obj:Any, fieldName:String):Any {
+ public static inline function getPrProp(obj: Any, fieldName: String): Any {
   return Syntax.code("{0}.@{1}@", proto(obj), (fieldName));
  }
 
  @:analyzer(local_dce)
- public static inline function setField(obj:Any, fieldName:String, value:Any) {
+ public static inline function setField(obj: Any, fieldName: String, value: Any) {
   return Syntax.code("{0} = {1}", Syntax.field(obj, fieldName), value);
  }
 
@@ -61,11 +61,11 @@ class Fn {
   * @param obj
   * @param fieldName
   */
- public static inline function getByArrSyntax(obj:Any, fieldName:String) {
+ public static inline function getByArrSyntax(obj: Any, fieldName: String) {
   return Syntax.code("{0}[{1}]", obj, fieldName);
  }
 
- public static inline function renameClass<T>(obj:Any, obj2:T):T {
+ public static inline function renameClass<T>(obj: Any, obj2: T): T {
   return Syntax.code("{0} = {1}", obj, obj2);
  }
 
@@ -76,7 +76,7 @@ class Fn {
   * @param value
   * @return T
   */
- public static function setProp<T:{}>(obj:T, propName:String, value:Any):T {
+ public static function setProp<T: {}>(obj: T, propName: String, value: Any): T {
   return Object.defineProperty(obj, propName, {value: value});
  }
 
