920c14d1a5d1ec58273dfca8d71d79db8f47f740
Kino  Rose <kinoarose@gmail.com>
2020-08-12 21:31:19 -0400

    Fix Game_Event abstract

--- src/mz/abstracts/objects/GameEvent.hx
+++ src/mz/abstracts/objects/GameEvent.hx
@@ -3,8 +3,10 @@ package mz.abstracts.objects;
 import mz.objects.Game_Enemy;
 import mz.objects.Game_Event;
 
-abstract GameEvent(Game_Event) from Game_Enemy to Game_Enemy {
- public inline function new() {
-  this = new Game_Event();
+@:forward
+@:forwardStatics
+abstract GameEvent(Game_Event) from Game_Event to Game_Event {
+ public inline function new(mapId: Int, eventId: Int) {
+  this = new Game_Event(mapId, eventId);
  }
 }
