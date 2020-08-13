f9870c7de6bd913b0f3b1b8fdf9fef9bcb89d3b4
Kino  Rose <kinoarose@gmail.com>
2020-08-12 21:26:00 -0400

    Add forwards to show underlying type fields

--- src/mz/abstracts/objects/GameAction.hx
+++ src/mz/abstracts/objects/GameAction.hx
@@ -3,6 +3,8 @@ package mz.abstracts.objects;
 import mz.objects.Game_Battler;
 import mz.objects.Game_Action;
 
+@:forward
+@:forwardStatics
 abstract GameAction(Game_Action) from Game_Action to Game_Action {
  public inline function new(subject: Game_Battler, forcing: Bool) {
   this = new Game_Action(subject, forcing);
