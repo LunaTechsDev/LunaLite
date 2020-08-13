83f80effc8865275369e573d23296691dc39ff54
Kino  Rose <kinoarose@gmail.com>
2020-08-12 22:26:05 -0400

    fix typo

--- src/mz/abstracts/sprites/SpriteBalloon.hx
+++ src/mz/abstracts/sprites/SpriteBalloon.hx
@@ -5,7 +5,7 @@ import mz.sprites.Sprite_Balloon;
 
 @:forward
 @:forwardStatics
-abstract SpriteBalloon(Sprite_Balloon) from Sprite_Balloon to Sprite_Ballon {
+abstract SpriteBalloon(Sprite_Balloon) from Sprite_Balloon to Sprite_Balloon {
  public inline function new() {
   this = new Sprite_Balloon();
  }
