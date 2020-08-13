a14cf2ad65d5a6c94f8163855e9ad1e9fa8bec92
Kino  Rose <kinoarose@gmail.com>
2020-08-12 21:20:34 -0400

    Create abstract and complete action types for MV

--- src/mz/abstracts/objects/GameAction.hx
+++ src/mz/abstracts/objects/GameAction.hx
@@ -1,1 +1,10 @@
 package mz.abstracts.objects;
+
+import mz.objects.Game_Battler;
+import mz.objects.Game_Action;
+
+abstract GameAction(Game_Action) from Game_Action to Game_Action {
+ public inline function new(subject: Game_Battler, forcing: Bool) {
+  this = new Game_Action(subject, forcing);
+ }
+}
