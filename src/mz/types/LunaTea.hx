c70ba9c4edb8292e50e26c5427796f2149595c78
Kino  Rose <kinoarose@gmail.com>
2020-08-12 22:21:06 -0400

    Inline the values as well

--- src/mz/types/LunaTea.hx
+++ src/mz/types/LunaTea.hx
@@ -13,15 +13,15 @@ enum abstract BuffId(Int) from Int to Int {
 @:forward
 @:forwardStatics
 enum abstract BalloonId(Int) from Int to Int {
- public var EXCLAMATION = 0;
- public var QUESTION = 1;
- public var MUSIC_NOTE = 2;
- public var HEART = 3;
- public var ANGER = 4;
- public var SWEAT = 5;
- public var COBWEB = 6;
- public var SILENCE = 7;
- public var ZZZ = 8;
+ public inline var EXCLAMATION = 0;
+ public inline var QUESTION = 1;
+ public inline var MUSIC_NOTE = 2;
+ public inline var HEART = 3;
+ public inline var ANGER = 4;
+ public inline var SWEAT = 5;
+ public inline var COBWEB = 6;
+ public inline var SILENCE = 7;
+ public inline var ZZZ = 8;
  // Extra Balloon IDs would be user defined
 }
 
