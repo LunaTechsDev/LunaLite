baf33da6ee35d890b72ae4cd7cd2fe6bfdf3df75
Kino  Rose <kinoarose@gmail.com>
2020-08-14 19:21:32 -0400

    Fill out Sprite Picture

--- src/mz/abstracts/sprites/SpritePicture.hx
+++ src/mz/abstracts/sprites/SpritePicture.hx
@@ -5,7 +5,7 @@ import mz.sprites.Sprite_Picture;
 @:forward
 @:forwardStatics
 abstract SpritePicture(Sprite_Picture) from Sprite_Picture to Sprite_Picture {
- public inline function new() {
-  this = new Sprite_Picture();
+ public inline function new(pictureId: Int) {
+  this = new Sprite_Picture(pictureId);
  }
 }
--- src/mz/sprites/Sprite_Picture.hx
+++ src/mz/sprites/Sprite_Picture.hx
@@ -1,6 +1,25 @@
 package mz.sprites;
 
+import mz.objects.Game_Picture;
+
+/**
+ * -----------------------------------------------------------------------------
+ * Sprite_Picture
+ *
+ * The sprite for displaying a picture.
+ */
 @:native("Sprite_Picture")
 extern class Sprite_Picture {
- public function new(): Void;
+ private var _pictureId: Int;
+ private var _pictureName: String;
+ private var _isPicture: Bool;
+ public function new(pictureId: Int);
+ public function picture(): Game_Picture;
+ public function updateBitmap(): Void;
+ public function updateOrigin(): Void;
+ public function updatePosition(): Void;
+ public function updateScale(): Void;
+ public function updateTone(): Void;
+ public function updateOther(): Void;
+ public function loadBitmap(): Void;
 }
