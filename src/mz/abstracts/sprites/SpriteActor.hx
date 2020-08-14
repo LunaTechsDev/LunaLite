4f353f4aad02f6db9409be4dacff439ef14cfeb7
Kino  Rose <kinoarose@gmail.com>
2020-08-14 19:05:30 -0400

    Add battler information to the SpriteActor abstract

--- src/mz/abstracts/sprites/SpriteActor.hx
+++ src/mz/abstracts/sprites/SpriteActor.hx
@@ -1,12 +1,13 @@
 package mz.abstracts.sprites;
 
+import mz.objects.Game_Actor;
 import mz.sprites.Sprite_Actor;
 
 @:forward
 @:forwardStatics
 abstract SpriteActor(Sprite_Actor) {
- public inline function new() {
-  this = new Sprite_Actor();
+ public inline function new(?battler: Game_Actor) {
+  this = new Sprite_Actor(battler);
  }
 
  public inline function raw(): Sprite_Actor {
