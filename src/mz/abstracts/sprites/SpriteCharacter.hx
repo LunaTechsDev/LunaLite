9a6e43209ab3cc0aa49344a3a46dd9c9fd5b42b4
Kino  Rose <kinoarose@gmail.com>
2020-08-14 18:37:25 -0400

    Add character patterns, update doc, add move frequency and fill out sprite character

--- src/mz/abstracts/sprites/SpriteCharacter.hx
+++ src/mz/abstracts/sprites/SpriteCharacter.hx
@@ -1,8 +1,16 @@
 package mz.abstracts.sprites;
 
+import mz.objects.Game_Character;
 import mz.sprites.Sprite_Character;
 
-abstract SpriteCharacter(Sprite_Character) {
+@:forward
+@:forwardStatics
+abstract SpriteCharacter(Sprite_Character) from Sprite_Character
+ to Sprite_Character {
+ public inline function new(character: Game_Character) {
+  this = new Sprite_Character(character);
+ }
+
  public inline function raw(): Sprite_Character {
   return this;
  }
--- src/mz/types/LunaTea.hx
+++ src/mz/types/LunaTea.hx
@@ -34,3 +34,56 @@ enum abstract AnimationSpeed(Int) from Int to Int {
 @:forwardStatics
 enum abstract WeaponImageId(Int) from Int to Int {
 }
+
+/**
+ * Character pattern represents the in-editor character
+ * pattern.
+ */
+@:forward
+@:forwardStatics
+enum abstract CharacterPattern(Int) from Int to Int {
+}
+
+/**
+ * The Move Types of Characters/Event in RPGMaker MV/MZ
+ */
+@:forward
+@:forwardStatics
+enum abstract MoveType(Int) from Int to Int {
+ public var FIXED = 0;
+ public var RANDOM = 1;
+ public var APPROACH = 2;
+ public var CUSTOM = 3;
+}
+
+/**
+ * The Move Speed Types of Characters/Events in RPGMaker MV/MZ
+ */
+@:forward
+@:forwardStatics
+enum abstract MoveSpeed(Int) from Int to Int {
+ public var X8SLOWER = 1;
+ public var X4SLOWER = 2;
+ public var X2SLOWER = 3;
+ public var NORMAL = 4;
+ public var X2FASTER = 5;
+ public var X4FASTER = 6;
+}
+
+@:forward
+@:forwardStatics
+enum abstract MoveFrequency(Int) from Int to Int {
+ public var LOWEST = 1;
+ public var LOWER = 2;
+ public var NORMAL = 3;
+ public var HIGHER = 4;
+ public var HIGHEST = 5;
+}
+
+@:forward
+@:forwardStatics
+enum abstract CharacterPriority(Int) from Int to Int {
+ public var BELOW_CHARACTERS = 0;
+ public var SAME_AS_CHARACTERS = 1;
+ public var ABOVE_CHARACTERS = 2;
+}
