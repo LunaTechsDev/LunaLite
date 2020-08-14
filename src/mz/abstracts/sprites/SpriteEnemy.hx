0473839f9994f7e52a46ba040c939b53d0246e61
Kino  Rose <kinoarose@gmail.com>
2020-08-14 19:26:50 -0400

    Add and fill out sprite enemy

--- src/mz/abstracts/sprites/SpriteEnemy.hx
+++ src/mz/abstracts/sprites/SpriteEnemy.hx
@@ -1,8 +1,15 @@
 package mz.abstracts.sprites;
 
+import mz.objects.Game_Enemy;
 import mz.sprites.Sprite_Enemy;
 
+@:forward
+@:forwardStatics
 abstract SpriteEnemy(Sprite_Enemy) {
+ public inline function new(battler: Game_Enemy) {
+  this = new Sprite_Enemy(battler);
+ }
+
  public inline function raw(): Sprite_Enemy {
   return this;
  }
