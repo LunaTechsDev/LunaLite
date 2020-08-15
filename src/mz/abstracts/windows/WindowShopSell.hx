9d5d5c8011ccae9e91b1036075496da9d2974278
Kino  Rose <kinoarose@gmail.com>
2020-08-15 08:49:52 -0400

    Add window shop sell abstract

---
+++ src/mz/abstracts/windows/WindowShopSell.hx
@@ -0,0 +1,12 @@
+package mz.abstracts.windows;
+
+import mz.windows.Window_ShopSell;
+
+@:forward
+@:forwardStatics
+abstract WindowShopSell(Window_ShopSell) from Window_ShopSell
+ to Window_ShopSell {
+ public inline function new(x: Int, y: Int, width: Int, height: Int) {
+  this = new Window_ShopSell(x, y, width, height);
+ }
+}
