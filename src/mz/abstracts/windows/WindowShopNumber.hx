fc4b9075d89d6c411de2f4c9daeecfb1af07f4b7
Kino  Rose <kinoarose@gmail.com>
2020-08-15 10:09:14 -0400

    Add forwards to Window_ShopNumber

--- src/mz/abstracts/windows/WindowShopNumber.hx
+++ src/mz/abstracts/windows/WindowShopNumber.hx
@@ -2,6 +2,8 @@ package mz.abstracts.windows;
 
 import mz.windows.Window_ShopNumber;
 
+@:forward
+@:forwardStatics
 abstract WindowShopNumber(Window_ShopNumber) from Window_ShopNumber
  to Window_ShopNumber {
  public inline function new(x: Int, y: Int, height: Int) {
