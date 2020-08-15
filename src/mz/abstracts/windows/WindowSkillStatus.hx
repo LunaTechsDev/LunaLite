97bf65313692fcf15098dd0b7b04e74620803223
Kino  Rose <kinoarose@gmail.com>
2020-08-15 16:25:52 -0400

    Add WIndowSkilLlist and WindowSkillStatus abstracts

---
+++ src/mz/abstracts/windows/WindowSkillStatus.hx
@@ -0,0 +1,12 @@
+package mz.abstracts.windows;
+
+import mz.windows.Window_SkillStatus;
+
+@:forward
+@:forwardStatics
+abstract WindowSkillStatus(Window_SkillStatus) from Window_SkillStatus
+ to Window_SkillStatus {
+ public inline function new(x: Int, y: Int, width: Int, height: Int) {
+  this = new Window_SkillStatus(x, y, width, height);
+ }
+}
