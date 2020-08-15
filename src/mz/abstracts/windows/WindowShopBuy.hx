75f027fe8c3bf7c9f886b8857abf5724cc5f7392
Kino  Rose <kinoarose@gmail.com>
2020-08-15 15:41:26 -0400

    Complete WindowSHopbuy abstract

--- src/mz/abstracts/windows/WindowShopBuy.hx
+++ src/mz/abstracts/windows/WindowShopBuy.hx
@@ -1,9 +1,12 @@
 package mz.abstracts.windows;
 
+import mz.windows.Window_ShopBuy;
+
 @:forward
 @:forwardStatics
 abstract WindowShopBuy(Window_ShopBuy) from Window_ShopBuy to Window_ShopBuy {
- public inline function new() {
-  this = new Window_ShopBuy();
+ public inline function new(x: Int, y: Int, height: Int,
+   shopGoods: Array<Array<Any>>) {
+  this = new Window_ShopBuy(x, y, height, shopGoods);
  }
 }
