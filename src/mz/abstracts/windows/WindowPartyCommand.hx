5e7bfd19d49c98639ea2dee3f37d4abcbb87f33e
Kino  Rose <kinoarose@gmail.com>
2020-08-15 08:31:41 -0400

    Add Window party command to the list of commands

---
+++ src/mz/abstracts/windows/WindowPartyCommand.hx
@@ -0,0 +1,12 @@
+package mz.abstracts.windows;
+
+import mz.windows.Window_PartyCommand;
+
+@:forward
+@:forwardStatics
+abstract WindowPartyCommand(Window_PartyCommand) from Window_PartyCommand
+ to Window_PartyCommand {
+ public inline function new() {
+  this = new Window_PartyCommand();
+ }
+}
