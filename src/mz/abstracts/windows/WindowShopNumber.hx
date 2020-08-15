0c14bcf05aae7981cef8b8aafbff87d385da09e5
Kino  Rose <kinoarose@gmail.com>
2020-08-15 09:02:00 -0400

    Add Window Shop Number to the list of windows

---
+++ src/mz/abstracts/windows/WindowShopNumber.hx
@@ -0,0 +1,10 @@
+package mz.abstracts.windows;
+
+import mz.windows.Window_ShopNumber;
+
+abstract WindowShopNumber(Window_ShopNumber) from Window_ShopNumber
+ to Window_ShopNumber {
+ public inline function new(x: Int, y: Int, height: Int) {
+  this = new Window_ShopNumber(x, y, height);
+ }
+}
