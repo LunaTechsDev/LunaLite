5dbafabb0f64f55d78c9febd93993e9a5faf5bd8
Kino  Rose <kinoarose@gmail.com>
2020-08-18 19:40:52 -0400

    Add conditional compilation for Window_ChoiceList

--- src/mz/abstracts/windows/WindowChoiceList.hx
+++ src/mz/abstracts/windows/WindowChoiceList.hx
@@ -7,8 +7,12 @@ import mz.windows.Window_ChoiceList;
 @:forwardStatics
 abstract WindowChoiceList(Window_ChoiceList) from Window_ChoiceList
  to Window_ChoiceList {
- public inline function new(messageWindow: Window_Message) {
+ public inline function new(?messageWindow: Window_Message) {
+  #if compileMV
   this = new Window_ChoiceList(messageWindow);
+  #else
+  this = new Window_ChoiceList();
+  #end
  }
 
  public inline function raw(): Window_ChoiceList {
--- src/mz/windows/Window_ChoiceList.hx
+++ src/mz/windows/Window_ChoiceList.hx
@@ -9,15 +9,26 @@ package mz.windows;
  */
 @:native("Window_ChoiceList")
 extern class Window_ChoiceList extends Window_Command {
-	private var _messageWindow:Window_Message;
-	private var _background:Int;
+ private var _messageWindow: Window_Message;
+ private var _background: Int;
 
-	public function new(messageWindow:Window_Message);
+ #if compileMV
+ public function new(messageWindow: Window_Message);
+ public function initialize(messageWindow: Window_Message): Void;
+ #else
+ public function new();
+ public function initialize(): Void;
+ public function setMessageWindow(messageWindow: Window_Message): Void;
+ public function windowY(): Int;
+ public function windowX(): Int;
+ public function needsCancelButton(): Bool;
+ public function maxLines(): Int;
+ #end
 
-	public function start():Void;
-	public function selectDefault():Void;
-	public function updatePlacement():Void;
-	public function updateBackground():Void;
-	public function maxChoiceWidth():Int;
-	public function textWidthEx(text:String):Int;
+ public function start(): Void;
+ public function selectDefault(): Void;
+ public function updatePlacement(): Void;
+ public function updateBackground(): Void;
+ public function maxChoiceWidth(): Int;
+ public function textWidthEx(text: String): Int;
 }
