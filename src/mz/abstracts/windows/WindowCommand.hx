61521a7f145d10f2e21dd4c183a0498b9feb58ea
Kino  Rose <kinoarose@gmail.com>
2020-08-18 19:53:49 -0400

    Add conditional compilation to Window_ScrollText

--- src/mz/abstracts/windows/WindowCommand.hx
+++ src/mz/abstracts/windows/WindowCommand.hx
@@ -1,12 +1,18 @@
 package mz.abstracts.windows;
 
+import mz.core.Rectangle;
 import mz.windows.Window_Command;
 
 @:forward
 @:forwardStatics
 abstract WindowCommand(Window_Command) from Window_Command to Window_Command {
  public inline function new(x: Int, y: Int) {
+  #if compileMV
   this = new Window_Command(x, y);
+  #else
+  var rect = new Rectangle(x, y);
+  this = new Window_Command(rect);
+  #end
  }
 
  public inline function raw(): Window_Command {
