ab903cae86de48c0db5f71549878340a69a62aaf
inc0der <inc0der.code@gmail.com>
2020-08-18 22:58:03 -0300

    Add missing methods in Scene_Title

--- src/mz/scenes/Scene_Title.hx
+++ src/mz/scenes/Scene_Title.hx
@@ -1,44 +1,56 @@
 package mz.scenes;
 
+import mz.core.Rectangle;
 import mz.core.Sprite;
 
 @:native("Scene_Title")
 extern class Scene_Title extends Scene_Base {
+ /**
+  * Creates the title scene background.
+  */
+ public function createBackground(): Void;
 
-  /**
-   * Creates the title scene background.
-   */
-  public function createBackground(): Void;
+ /**
+  * Creates the title screen foreground.
+  */
+ public function createForeground(): Void;
 
-  /**
-   * Creates the title screen foreground.
-   */
-  public function createForeground():Void;
+ /**
+  * Draws the game title.
+  */
+ public function drawGameTitle(): Void;
 
-  /**
-   * Draws the game title.
-   */
-  public function drawGameTitle():Void;
+ /**
+  * Scales the background images and centers it.
+  */
+ public function adjustBackground(): Void;
 
-  public function centerSprite(sprite:Sprite):Void;
+ /**
+  * Creates the command window and sets it's handlers.
+  */
+ public function createCommandWindow(): Void;
 
-  /**
-   * Handler for the new game command.
-   */
-  public function commandNewGame():Void;
+ public function commandWindowRect(): Rectangle;
 
-  /**
-   * Handler for the continue command.
-   */
-  public function commandContinue(): Void;
+ public function centerSprite(sprite: Sprite): Void;
 
-  /**
-   * Handler for the options command.
-   */
-  public function commandOptions():Void;
+ /**
+  * Handler for the new game command.
+  */
+ public function commandNewGame(): Void;
 
-  /**
-   * Plays the title screen music upon entering the scene.
-   */
-  public function playTitleMusic():Void;
+ /**
+  * Handler for the continue command.
+  */
+ public function commandContinue(): Void;
+
+ /**
+  * Handler for the options command.
+  */
+ public function commandOptions(): Void;
+
+ /**
+  * Plays the title screen music upon entering the scene.
+  */
+ public function playTitleMusic(): Void;
 }
