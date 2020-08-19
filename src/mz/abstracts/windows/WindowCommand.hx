e46c86da0504c2a56d29411772cadc25f01e05d3
Kino  Rose <kinoarose@gmail.com>
2020-08-18 20:36:26 -0400

    Add conditional compilation to window creation for Window_Command, WindowCommand

--- src/mz/abstracts/windows/WindowCommand.hx
+++ src/mz/abstracts/windows/WindowCommand.hx
@@ -6,11 +6,20 @@ import mz.windows.Window_Command;
 @:forward
 @:forwardStatics
 abstract WindowCommand(Window_Command) from Window_Command to Window_Command {
- public inline function new(x: Int, y: Int) {
+ /**
+  * Creates a new Window_Command.
+  * Takes x, y, width, height.
+  * Width and height are optional and for MZ.
+  * @param x
+  * @param y
+  * @param width
+  * @param height
+  */
+ public inline function new(x: Int, y: Int, ?width: Int, ?height: Int) {
   #if compileMV
   this = new Window_Command(x, y);
   #else
-  var rect = new Rectangle(x, y);
+  var rect = new Rectangle(x, y, width, height);
   this = new Window_Command(rect);
   #end
  }
