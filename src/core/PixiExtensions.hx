0e6c9ef203cfa4c12b6b8cee046794f6e973a1b7
Kino  Rose <kinoarose@gmail.com>
2020-08-19 21:16:03 -0400

    Add one additional method for pixi extensions

--- src/core/PixiExtensions.hx
+++ src/core/PixiExtensions.hx
@@ -1,4 +1,15 @@
 package core;
 
+import pixi.core.display.DisplayObject;
+
 class PixiExtensions {
+ /**
+  * Sets the pixi object interactable along with
+  * setting the button mode to interactable as well.
+  * @param bool
+  */
+ public static function setInteracble(displayObj: DisplayObject, bool: Bool) {
+  displayObj.interactive = bool;
+  displayObj.buttonMode = bool;
+ }
 }
