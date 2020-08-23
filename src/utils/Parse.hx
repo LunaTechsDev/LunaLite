29a13f36b9f75ffeacb943092e668deee73c67bc
Kino  Rose <kinoarose@gmail.com>
2020-08-23 13:58:56 -0400

    recommit change

--- src/utils/Parse.hx
+++ src/utils/Parse.hx
@@ -9,7 +9,7 @@ class Parse {
  /**
   * Function for parsing parameters from JSON in MV/MZ
   */
- public inline static function parseParameters(parameters: String): Any {
+ public static function parseParameters(parameters: String): Any {
   var obj: DynamicAccess<Dynamic>;
   try {
    obj = JsonEx.parse(parameters);
