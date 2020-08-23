c2a098737f7c2cb43bea862eebea12fdad86378f
Kino  Rose <kinoarose@gmail.com>
2020-08-23 14:46:38 -0400

    Update parseParameters

--- src/utils/Parse.hx
+++ src/utils/Parse.hx
@@ -10,25 +10,47 @@ class Parse {
   * Function for parsing parameters from JSON in MV/MZ
   */
  public static function parseParameters(parameters: String): Any {
-  var obj: DynamicAccess<Dynamic>;
-  try {
-   obj = JsonEx.parse(parameters);
-  } catch (e) {
-   trace(e);
-   return parameters;
-  }
-  trace(Syntax.typeof(obj));
-  if (isObject(obj)) {
-   for (key in Object.keys(cast obj)) {
-    //  var dynamicObj: DynamicAccess<Dynamic> = obj;
-    obj[key] = parseParameters(obj[key]);
-    if (obj[key] == "") {
-     obj[key] = null;
+  return Syntax.code("var JSONSuperParse = function (string) {
+    var temp;
+    try {
+        temp = JsonEx.parse(typeof string === '
+   object ' ? JsonEx.stringify(string) : string);
+    } catch (e) {
+        return string;
     }
-   }
-  }
-  return obj;
+    if (typeof temp === '
+   object ') {
+        Object.keys(temp).forEach(function (key) {
+            temp[key] = JSONSuperParse(temp[key]);
+            if (temp[key] === ' ') {
+                temp[key] = null;
+            }
+        });
+    }
+    return temp;
+};JSONSuperParse({0})", parameters);
  }
+
+ //  public static function parseParameters(parameters: String): Any {
+ //   var obj: DynamicAccess<Dynamic>;
+ //   try {
+ //    obj = JsonEx.parse(parameters);
+ //   } catch (e) {
+ //    trace(e);
+ //    return parameters;
+ //   }
+ //   trace(Syntax.typeof(obj));
+ //   if (isObject(obj)) {
+ //    for (key in Object.keys(cast obj)) {
+ //     //  var dynamicObj: DynamicAccess<Dynamic> = obj;
+ //     obj[key] = parseParameters(obj[key]);
+ //     if (obj[key] == "") {
+ //      obj[key] = null;
+ //     }
+ //    }
+ //   }
+ //   return obj;
+ //  }
 
  public static function isObject(entity: Dynamic) {
   return Syntax.typeof(entity) == "Object";
