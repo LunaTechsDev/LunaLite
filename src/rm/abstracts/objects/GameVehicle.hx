72b4bca45de14e7b1f739c917bb2f4c295a997e5
Kino  Rose <kinoarose@gmail.com>
2020-08-24 22:45:49 -0400

    Did compatibility check

--- src/rm/abstracts/objects/GameVehicle.hx
+++ src/rm/abstracts/objects/GameVehicle.hx
@@ -5,7 +5,7 @@ import rm.objects.Game_Vehicle;
 @:forward
 @:forwardStatics
 abstract GameVehicle(Game_Vehicle) from Game_Vehicle to Game_Vehicle {
- public inline function new() {
-  this = new Game_Vehicle();
+ public inline function new(type: String) {
+  this = new Game_Vehicle(type);
  }
 }
