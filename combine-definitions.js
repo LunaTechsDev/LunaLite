9c0d91841bd9462ea761875953c4277b93313e2e
Kino  Rose <kinoarose@gmail.com>
2020-08-25 19:27:35 -0400

    Update Documentation Information and git workflow

---
+++ combine-definitions.js
@@ -0,0 +1,16 @@
+const fs = require("fs")
+
+
+let combineFiles = (file1, file2, outputFile) => {
+  const fileContent = fs.readFileSync(file1, "utf8")
+  const fileContent2 = fs.readFileSync(file2, "utf8")
+
+  fs.writeFileSync(outputFile, fileContent + fileContent2, "utf8")
+}
+
+
+combineFiles("definitions/pixi-4.5.4.d.ts", "definitions/lunalite-mv.d.ts", "definitions/lunalite-pixi-mv.d.ts")
+combineFiles("definitions/pixi-5.2.4.js.d.ts", "definitions/lunalite-mz.d.ts", "definitions/lunalite-pixi-mz.d.ts")
+
+
+
